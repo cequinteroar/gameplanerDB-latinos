c4131493419d2d148c0f7fc9c1f5e7fbcbf1bdd5
Cesar Quintero Arias <cequinteroar@unal.edu.co>
2024-05-26 02:17:54 +0100

    removed unused code

--- server.ts
+++ server.ts
@@ -47,38 +47,3 @@ app.use(bodyParser.json()); // Convierte a json lo que llega por peticiones
     console.log("could not connect to MongoDB", error);
   }
 })();
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client: MongoClient = new MongoClient(CONNECTION_STRING, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-//     app.listen(PORT, function () {
-//       console.log("Game planner server Up" + PORT);
-//     });
-
-//     //Pruebas
-//     app.get("/pruebas", (req, res) => {
-//       res.json({ message: "Game planner server Up" });
-//     });
-
-//     //  Base routes
-//     app.use("/gameplanner", UserRoutes);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
